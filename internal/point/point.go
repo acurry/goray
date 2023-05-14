c5f9821378d6e9a711fd594195d9799d608332f8
Andrew Curry <acurry@users.noreply.github.com>
2023-05-14 16:10:33 -0400

    Add helper funcs for Point methods

--- internal/point/point.go
+++ internal/point/point.go
@@ -23,6 +23,18 @@ func (a *Point) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	return nil
 }
 
+func Dot(a *Point, b *Point) float64 {
+	return (a.x * b.x) + (a.y * b.y) + (a.z * b.z)
+}
+
+func Cross(a *Point, b *Point) *Point {
+	return &Point{
+		(a.y * b.z) - (a.z * b.y),
+		(a.z * b.x) - (a.x * b.z),
+		(a.x * b.y) - (a.y * b.x),
+	}
+}
+
 func (a *Point) Dot(b *Point) float64 {
 	return (a.x * b.x) + (a.y * b.y) + (a.z * b.z)
 }
@@ -48,6 +60,20 @@ func (a *Point) Normalize() *Point {
 	}
 }
 
+func Reflected(vector *Point, axis *Point) *Point {
+	/* return vector - 2 * np.dot(vector, axis) * axis */
+	x := Mul(axis, 2*Dot(vector, axis))
+	return Sub(vector, x)
+}
+
+func Add(a *Point, b *Point) *Point {
+	return &Point{
+		a.x + b.x,
+		a.y + b.y,
+		a.z + b.z,
+	}
+}
+
 func (a *Point) Add(b *Point) *Point {
 	return &Point{
 		a.x + b.x,
@@ -56,11 +82,43 @@ func (a *Point) Add(b *Point) *Point {
 	}
 }
 
+func Sub(a *Point, b *Point) *Point {
+	return &Point{
+		a.x - b.x,
+		a.y - b.y,
+		a.z - b.z,
+	}
+}
+
 func (a *Point) Sub(b *Point) *Point {
 	return &Point{
 		a.x - b.x,
 		a.y - b.y,
 		a.z - b.z,
+	}
+}
+
+func Mul(a *Point, x float64) *Point {
+	return &Point{
+		a.x * x,
+		a.y * x,
+		a.z * x,
+	}
+}
+
+func (a *Point) Mul(x float64) *Point {
+	return &Point{
+		a.x * x,
+		a.y * x,
+		a.z * x,
+	}
+}
+
+func Div(a *Point, x float64) *Point {
+	return &Point{
+		a.x / x,
+		a.y / x,
+		a.z / x,
 	}
 }
 
