27502d9bd5f7a7c540c3b6720d759ac26a6301c1
Andrew Curry <acurry@users.noreply.github.com>
2023-06-17 15:21:55 -0400

    clean up code before integrating The Ray Tracer Challenge PDF

--- cmd/main.go
+++ cmd/main.go
@@ -7,24 +7,20 @@ import (
 	"log"
 	"os"
 
-	"github.com/acurry/goray/internal/point"
 	"github.com/acurry/goray/internal/scene"
-	"github.com/acurry/goray/internal/utils"
 	"gopkg.in/yaml.v3"
 )
-
-const version = "1.0.0"
 
 type Root struct {
 	Scene scene.Scene
 }
 
-func save(filePath string, img *image.NRGBA) {
+func Save(filePath string, img *image.NRGBA) {
 	imgFile, err := os.Create(filePath)
-	defer imgFile.Close()
 	if err != nil {
 		log.Println("Cannot create file:", err)
 	}
+	defer imgFile.Close()
 	png.Encode(imgFile, img.SubImage(img.Rect))
 }
 
@@ -42,20 +38,6 @@ func main() {
 	}
 
 	root.Scene.Init()
-
-	rect := image.Rect(0, 0, int(root.Scene.Width), int(root.Scene.Height))
-
-	pix := make([]uint8, rect.Dx()*rect.Dy()*4)
-
-	outer := utils.LinSpace(root.Scene.Screen.Top, root.Scene.Screen.Bottom, root.Scene.Height)
-	inner := utils.LinSpace(root.Scene.Screen.Left, root.Scene.Screen.Right, root.Scene.Width)
-
-	for i, x := range outer {
-		for j, y := range inner {
-			pixel := point.Point{X: x, Y: y, Z: 0.0}
-			direction := pixel.Normalize()
-		}
-	}
 
 	// Print out the new struct
 	fmt.Printf("%+v\n", root)
